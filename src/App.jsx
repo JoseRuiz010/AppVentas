b15fd79ed01053cad4ecc51390a0e9053dd37a8a
joseruiz010 <joruiz765@gmail.com>
2023-04-15 18:17:24 -0300

    instale Router-dom, tailwind, daisyUI, finalForm

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-      <h1>App Venta</h1>
-    </div>
+    <h1 className="text-4xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
