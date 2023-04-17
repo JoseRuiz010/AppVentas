8938c8edbea34e3c37921f4726f9f227f47635b3
joseruiz010 <joruiz765@gmail.com>
2023-04-17 09:12:36 -0300

    get Products dede el API

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { useEffect } from 'react';
 import './App.css'
-
+import { encode, decode } from 'base-64'
 function App() {
+  const headers = new Headers();
+  // headers.append("Authorization", "Basic " + encode("admin:1234"))
+  // console.log("Authorization", "Basic " + encode("admin:1234"))
+
+  // useEffect(() => {
+  //   getData();
+  // }, [])
+
+  // const getData = async () => {
+  //   const res = await fetch('http://localhost:8090/producto/10'
+  //     , { headers: { "Authorization": "Basic " + encode("admin:1234") } })
+  //   const data = await res.json()
+  //   console.log(data)S
+  // }
 
   return (
     <h1 className="text-4xl font-bold underline">
