22b7ba8144ac16a0c534e7d4a98d354a3f73fe64
joseruiz010 <joruiz765@gmail.com>
2023-04-20 00:40:05 -0300

    React data table

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import './App.css'
 import { encode, decode } from 'base-64'
+import { Tabla } from './components/Tabla';
 function App() {
   const headers = new Headers();
   // headers.append("Authorization", "Basic " + encode("admin:1234"))
@@ -20,6 +21,7 @@ function App() {
   return (
     <h1 className="text-4xl font-bold underline">
       Hello world!
+      <Tabla />
     </h1>
   )
 }
